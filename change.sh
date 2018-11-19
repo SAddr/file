6f36596f9eccfffdd70c11da68b0b620b207ff0c
SAddr <wliao1@qq.com>
2018-11-19 11:51:30 +0800

    Create change.sh

---
+++ change.sh
@@ -0,0 +1,6 @@
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base2.repo_bak;
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo;
+yum makecache;
+yum isntall -y kernel-devel kernel-devel-3.10.0-862.e17.x86_64;
+mv /etc/yum.repos.d/CentOS-Base2.repo_bak /etc/yum.repos.d/CentOS-Base.repo;
+yum makecache;
